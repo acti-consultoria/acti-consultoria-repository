88550f3b26acc598de411732efce56086b55ef87
Leonardo Sarmento de Castro <leonardo.castro@acticonsultoria.com.br>
2014-07-15 16:55:21 -0300

    Criação de POM e Jetty imbutido - nao finalizado

---
+++ Project/acti-web/src/main/java/com/br/acti/LoginController.java
@@ -0,0 +1,21 @@
+package com.br.acti;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+	@RequestMapping(value = "/form", method = RequestMethod.GET)
+    public String index(Model model ) {
+		
+		model.addAttribute("acti", "valor do parâmetro");
+        return "form";
+    }
+ 
+
+	
+}
